09e9e6e315be24942ce43043e4cbf204bfa803b1
mostafa <Mustafa2nady@gmail.com>
2025-11-01 20:08:28 +0200

    update Main

--- src/main/java/org/example/TestGitHubAnalyzer.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class TestGitHubAnalyzer {
-    public static void main(String[] args) {
-        GitHubAnalyzer analyzer = new GitHubAnalyzer();
-        String username1 = "MostafaNady2";
-        analyzer.analyze(username1);
-    }
-}
