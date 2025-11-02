d50bb91145da9e3637f6b8c8044b6ddaad3cc08b
mostafa <Mustafa2nady@gmail.com>
2025-11-02 18:18:42 +0200

    refactoring [formatting]

--- src/main/java/org/example/GitHubAnalyzer.java
+++ src/main/java/org/example/GitHubAnalyzer.java
@@ -14,143 +14,145 @@ import java.util.Map;
 import java.util.stream.StreamSupport;
 
 public class GitHubAnalyzer {
-    private static HttpURLConnection conn = null;
-    private static ObjectMapper mapper = new ObjectMapper();
+	private static HttpURLConnection conn = null;
+	private static ObjectMapper mapper = new ObjectMapper();
 
-    public void analyze(String username) {
-        System.out.printf("=== GitHub Profile: %s ===\n\n", username);
-        analyzeProfile(username);
-        analyzeRepositories(username);
-        analyzeLanguages(username);
-    }
+	public void analyze(String username) {
+		System.out.printf("=== GitHub Profile: %s ===\n\n", username);
+		analyzeProfile(username);
+		analyzeRepositories(username);
+		analyzeLanguages(username);
+	}
 
-    private static String getData(String urlStr) {
-        HttpURLConnection conn = null;
-        try {
-            URL url = new URL(urlStr);
+	private static String getData(String urlStr) {
+		HttpURLConnection conn = null;
+		try {
+			URL url = new URL(urlStr);
 
-            conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("User-Agent", "Java-GitHubAnalyzer");
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("User-Agent", "Java-GitHubAnalyzer");
 
-            int responseCode = conn.getResponseCode();
-            if (responseCode == 200) {
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-                    String line;
-                    StringBuilder sb = new StringBuilder();
-                    while ((line = reader.readLine()) != null) {
-                        sb.append(line);
-                    }
-                    if (conn != null) {
-                        conn.disconnect();
-                    }
-                    return sb.toString();
-                }
-            }
-        } catch (Exception e) {
-        }finally {
-            if (conn != null) {
-                conn.disconnect();
-            }
-        }
-        return null;
-    }
+			int responseCode = conn.getResponseCode();
+			if (responseCode == 200) {
+				try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+					String line;
+					StringBuilder sb = new StringBuilder();
+					while ((line = reader.readLine()) != null) {
+						sb.append(line);
+					}
+					if (conn != null) {
+						conn.disconnect();
+					}
+					return sb.toString();
+				}
+			}
+		} catch (Exception e) {
+		} finally {
+			if (conn != null) {
+				conn.disconnect();
+			}
+		}
+		return null;
+	}
 
-    public static void analyzeProfile(String username) {
+	public static void analyzeProfile(String username) {
 
-        String urlStr = "https://api.github.com/users/" + username;
-        String json = getData(urlStr);
-        if(!isValid(json)){
-            return;
-        }
-        JsonNode root = null;
-        try {
-            root = mapper.readTree(json);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("Name : " + root.get("name").asText("No Name"));
-        System.out.println("Bio : " + root.get("bio").asText("No Bio"));
-        System.out.println("Followers : " + root.get("followers").asText("No Followers"));
-        System.out.println("Following : " + root.get("following").asText("No Following"));
-        System.out.println("Public Repos: +" + root.get("public_repos").asText("No Public Repos"));
-    }
+		String urlStr = "https://api.github.com/users/" + username;
+		String json = getData(urlStr);
+		if (!isValid(json)) {
+			return;
+		}
+		JsonNode root = null;
+		try {
+			root = mapper.readTree(json);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+		System.out.println("Name : " + root.get("name").asText("No Name"));
+		System.out.println("Bio : " + root.get("bio").asText("No Bio"));
+		System.out.println("Followers : " + root.get("followers").asText("No Followers"));
+		System.out.println("Following : " + root.get("following").asText("No Following"));
+		System.out.println("Public Repos: +" + root.get("public_repos").asText("No Public Repos"));
+	}
 
-    public static void analyzeRepositories(String username) {
-        String urlStr = "https://api.github.com/users/" + username + "/repos?per_page=100";
-        String json = getData(urlStr);
+	public static void analyzeRepositories(String username) {
+		String urlStr = "https://api.github.com/users/" + username + "/repos?per_page=100";
+		String json = getData(urlStr);
 
-        if(!isValid(json)){
-            return;
-        }
+		if (!isValid(json)) {
+			return;
+		}
 
-        JsonNode root = null;
-        try {
-            root = mapper.readTree(json);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("\nTop 3 Starred Repos:");
-        // to use root as stream
-        StreamSupport.stream(root.spliterator(), false).sorted((r1, r2) -> {
-            return r2.get("stargazers_count").asInt() - r1.get("stargazers_count").asInt();
-        }).limit(3).forEach((repo) -> {
-            System.out.printf("- %s (Stars : %s)\n", repo.get("name").asText("No Name"), repo.get("stargazers_count").asInt(0));
-        });
-        System.out.println();
-    }
+		JsonNode root = null;
+		try {
+			root = mapper.readTree(json);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+		System.out.println("\nTop 3 Starred Repos:");
+		// to use root as stream
+		StreamSupport.stream(root.spliterator(), false).sorted((r1, r2) -> {
+			return r2.get("stargazers_count").asInt() - r1.get("stargazers_count").asInt();
+		}).limit(3).forEach((repo) -> {
+			System.out.printf("- %s (Stars : %s)\n", repo.get("name").asText("No Name"),
+					repo.get("stargazers_count").asInt(0));
+		});
+		System.out.println();
+	}
 
-    public static void analyzeLanguages(String username) {
+	public static void analyzeLanguages(String username) {
 
-        String urlStr = "https://api.github.com/users/" + username + "/repos";
-        String urlJson = getData(urlStr);
-        if(!isValid(urlJson)){
-            return;
-        }
+		String urlStr = "https://api.github.com/users/" + username + "/repos";
+		String urlJson = getData(urlStr);
+		if (!isValid(urlJson)) {
+			return;
+		}
 
-        JsonNode root = null;
-        try {
-            root = mapper.readTree(urlJson);
-        } catch (JsonProcessingException e) {
-        }
-        Map<String,Long> map = new HashMap<>();
+		JsonNode root = null;
+		try {
+			root = mapper.readTree(urlJson);
+		} catch (JsonProcessingException e) {
+		}
+		Map<String, Long> map = new HashMap<>();
 
-        StreamSupport.stream(root.spliterator(), false).forEach((repo) -> {
-            JsonNode url = repo.get("languages_url");
-            String str = url.asText();
-            //System.out.println(str);  // url of repo
+		StreamSupport.stream(root.spliterator(), false).forEach((repo) -> {
+			JsonNode url = repo.get("languages_url");
+			String str = url.asText();
+			// System.out.println(str); // url of repo
 
-            String data = getData(str);   // date retrieved from repo languages Link
-            if(!isValid(data)){
-                return;
-            }
+			String data = getData(str); // date retrieved from repo languages Link
+			if (!isValid(data)) {
+				return;
+			}
 
-            try {
-                JsonNode languagesNode = mapper.readTree(data);
-                Iterator<String> lang =  languagesNode.fieldNames();
-                String langName;
-                long val;
-                while (lang.hasNext()) {
-                    langName = lang.next();
-                    val = map.getOrDefault(langName,0L);
-                    map.put(langName, val + languagesNode.get(langName).asLong(0L));
-                }
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        final long sum = map.values().stream().reduce(0L, (a,b) -> a + b);
-        System.out.println("Languages Used:");
-        map.forEach((key, value) -> {
-            double percentage = (double) value / sum * 100.0;
-            System.out.printf("- %s: %.2f%%\n", key, percentage);
-        });
-        System.out.println();
-    }
-    private static boolean isValid(String json){
-        if(json == null || json.trim().isEmpty() || json.isBlank()){
-            return false;
-        }
-        return true;
-    }
+			try {
+				JsonNode languagesNode = mapper.readTree(data);
+				Iterator<String> lang = languagesNode.fieldNames();
+				String langName;
+				long val;
+				while (lang.hasNext()) {
+					langName = lang.next();
+					val = map.getOrDefault(langName, 0L);
+					map.put(langName, val + languagesNode.get(langName).asLong(0L));
+				}
+			} catch (JsonProcessingException e) {
+				throw new RuntimeException(e);
+			}
+		});
+		final long sum = map.values().stream().reduce(0L, (a, b) -> a + b);
+		System.out.println("Languages Used:");
+		map.forEach((key, value) -> {
+			double percentage = (double) value / sum * 100.0;
+			System.out.printf("- %s: %.2f%%\n", key, percentage);
+		});
+		System.out.println();
+	}
+
+	private static boolean isValid(String json) {
+		if (json == null || json.trim().isEmpty() || json.isBlank()) {
+			return false;
+		}
+		return true;
+	}
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,14 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        if(args.length==0){
-            System.out.println("Usage : java Main <github-username>");
-            System.out.println("Example : java Main torvalds");
-            return;
-        }
-        String username = args[0];
-        GitHubAnalyzer analyzer = new GitHubAnalyzer();
-        analyzer.analyze(username);
-    }
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Usage : java Main <github-username>");
+			System.out.println("Example : java Main torvalds");
+			return;
+		}
+		String username = args[0];
+		GitHubAnalyzer analyzer = new GitHubAnalyzer();
+		analyzer.analyze(username);
+	}
 }
